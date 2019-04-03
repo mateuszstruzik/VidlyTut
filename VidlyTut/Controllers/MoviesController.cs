f8d90b683181f015a12ffa894c2038d26b15e4ea
Mateusz Struzik <matistruzik@gmail.com>
2019-04-03 15:57:25 +0200

    views part tutorial

--- VidlyTut/Controllers/MoviesController.cs
+++ VidlyTut/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidlyTut.Models;
+using VidlyTut.ViewModels;
 
 namespace VidlyTut.Controllers
 {
@@ -13,10 +14,23 @@ namespace VidlyTut.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customer = new List<Customer>
+            {
+                new Customer{Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewmodel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
         }
-
+        
+        //ASP.NET MVC Attribute Route Constraints 
+        [Route("movies/released/{year}/{month}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year+"/"+month);
---
+++ VidlyTut/ViewModels/RandomMovieViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyTut.Models;
+
+namespace VidlyTut.ViewModels
+{
+    public class RandomMovieViewmodel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
